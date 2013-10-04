483a81d85d42f0f5277a11ba1b586977035fb23a
ifkite <if.kite@gmail.com>
2013-10-04 13:12:48 +0800

    v0.1 may come out soon

--- poj-login.py
+++ poj-login.py
@@ -4,35 +4,50 @@ import gzip
 import StringIO
 import sys
 import re
+import cookielib
 #import httplib
 #httplib.HTTPConnection.debuglevel=1
-def login(postUrl, postData):
-    request = urllib2.Request(postUrl, urllib.urlencode(postData))
+'''change agent'''
+def postData(logUrl, logData):
+    request = urllib2.Request(logUrl, urllib.urlencode(logData))
+    #request.add_header('Referer',referer)
+    request.add_header('host','poj.org')
+    request.add_header('User-Agent','Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:10.0.1) Gecko/20100101 Firefox/10.0.1')
     request.add_header('Accept-encoding', 'gzip')
     request.add_header('Connection','keep-alive')
-    opener = urllib2.build_opener()
+    cj = cookielib.CookieJar()
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+    '''another way to add header'''
+    #opener.addheaders = [(),(),]
     gzipFd = opener.open(request)
     gzipData = gzipFd.read()
     gzipStream = StringIO.StringIO(gzipData)
     unZipFd = gzip.GzipFile(fileobj=gzipStream)
     html = unZipFd.read() 
-    open('tmp.html','w').write(html)
+    #open('tmp.html','w').write(html)
+    return html
     
 def submit(subUrl, subData):
     request = urllib2.Request()
 
 def main():
-    postUrl = 'http://poj.org/login'
+    '''login'''
+    logUrl = 'http://poj.org/submit'
     psWord = raw_input('Enter your password in POJ: ')
-    postData = {'user_id1':'McDolphin', 'password1':psWord, 'url':'/', 'B1':login}
-    #login(postUrl, postData)
-     
+    srcCode = ''
+    problem_id = '1000'
+    logData = {'user_id1':'testdot', 'password1':psWord, 'url':'/', 'B1':'login','language':'1', 'problem_id':problem_id, 'source':srcCode, 'submit':'Submit'}
+    #referer = 'http://poj.org/submit?problem_id=1000'
+    logHtml = postData(logUrl, logData)
+    open('login.html','w').write(logHtml)
     '''
        1.get name from args
        2.parsing, get the problem_id
        3.read specific file
        4.submit file to matched url
     ''' 
+
+    '''
     pattern = '(\d+)\.(\w+)'
     for v in sys.argv[1:]:
         matchobj = re.match(pattern, v)
@@ -43,9 +58,11 @@ def main():
                 print 'failed to open %s, file may not exist' %(v),
             else:
                 problem_id = matchobj.group(1)
-                submitUrl = 'poj.org/problem?id='+problem_id
+                submitUrl = 'http://poj.org/submit'
                 srcCode = srcFile.read()
-                submitData = {'language':'1', 'problem_id':problem_id, 'source':srcCode, 'submit':Submit}
-                submit(submitUrl, submitData)
+                submitData = {'language':'1', 'problem_id':problem_id, 'source':srcCode, 'submit':'Submit'}
+                html = postData(submitUrl, submitData)
+                open('submit.html','w').write(html) 
+     '''
 if __name__ == '__main__':
     main()
